6f85bc55d67912cafee1374e1f9d3a1f88c4121d
seandoerr <seandoerr@10.1.86.124>
2016-05-09 14:23:41 -0700

    Game without scrolling background

--- DarkSoulsFour/MainPackage/Game.java
+++ DarkSoulsFour/MainPackage/Game.java
@@ -63,24 +63,23 @@ public class Game extends JPanel implements ActionListener
 		super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
 		
-		int temp = person.nx;
-		if((person.getX() - 110) % 1280 == 0) 
+		/*if((person.getX() - 110) % 1280 == 0) 
 		{
 			person.nx = 0;
 		}
 		if((person.getX() - 750) % 1280 == 0)
 		{
 			person.nx2 = 0;
-		}
+		}*/
 		
-		g2d.drawImage(img,622-person.nx2, 0, null);
+		g2d.drawImage(img,0/*622-person.nx2*/, 0, null);
 		//System.out.println(person.getX());
 		
-		if(person.getX() >= 110)
+		/*if(person.getX() >= 110)
 		{
 			g2d.drawImage(img,622-person.nx, 0, null);
-		}
-		g2d.drawImage(person.getImage(), 110, person.getY() ,null);
+		}*/
+		g2d.drawImage(person.getImage(), person.getX(), person.getY() ,null);
 		
 		
 		
@@ -88,13 +87,13 @@ public class Game extends JPanel implements ActionListener
 		for(int i = 0; i < projectiles.size(); i++)
 		{
 			Projectile p = (Projectile) projectiles.get(i);
-			g2d.drawImage(img2, p.getX() - person.nx, p.getY(), null);
+			g2d.drawImage(img2, p.getX() /*- person.nx*/, p.getY(), null);
 			
-			if(i == 0)
+			/*if(i == 0)
 			{
 				System.out.println("Projectile: " + (p.getX() - person.nx));
 				System.out.println("Person: " + person.getX());
-			}
+			}*/
 		}
 		
 		g2d.drawImage(enemy.getImage(), enemy.getX(), enemy.getY(), null);
